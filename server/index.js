f98286a5aaf69df87f35478ec7887a71faee63c7
zaid2046 <148091182+zaid2046@users.noreply.github.com>
2024-05-27 08:57:13 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use("/users", userRoutes)
 const PORT = 3001;
 mongoose
   .connect(process.env.MONGO_URL, {
-    dbName: "Dream_Nest",
+    dbName: "rentify-project",
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
