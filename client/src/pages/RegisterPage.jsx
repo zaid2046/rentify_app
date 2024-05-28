f117236e0a638436137a962d88bad72ae2e0177c
zaid2046 <148091182+zaid2046@users.noreply.github.com>
2024-05-28 15:11:12 +0530

    Update RegisterPage.jsx

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -40,7 +40,7 @@ const RegisterPage = () => {
         register_form.append(key, formData[key])
       }
 
-      const response = await fetch("/auth/register", {
+      const response = await fetch("https://rentify-backend-three.vercel.app/auth/register", {
         method: "POST",
         body: register_form
       })
