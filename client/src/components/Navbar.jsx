c60dc1c66987d0ee8079d9e50812859b07e0b238
zaid2046 <148091182+zaid2046@users.noreply.github.com>
2024-05-28 15:03:10 +0530

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -60,7 +60,7 @@ const Navbar = () => {
             <Person sx={{ color: variables.darkgrey }} />
           ) : (
             <img
-              src={`/${user.profileImagePath.replace(
+              src={`https://rentify-backend-three.vercel.app/${user.profileImagePath.replace(
                 "public",
                 ""
               )}`}
